3a3f111bf0d8cf2e8780c709a7d5a969a01214f4
Ivo Biaus <ivobiaus@gmail.com>
2022-02-14 17:42:39 -0300

    Changed BrowserRouter for HashRouter in an attempt to fix the deployed site navigation.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "antd/dist/antd.min.css";
 
@@ -13,9 +13,9 @@ import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
